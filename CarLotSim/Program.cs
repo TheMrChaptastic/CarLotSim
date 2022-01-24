eab6f96b092a88c0874c2a6fb36767c198162f0d
TheMrChaptastic <chappy@themrchaptastic.com>
2022-01-24 14:59:10 -0600

    added few lines

--- CarLotSim/Program.cs
+++ CarLotSim/Program.cs
@@ -9,7 +9,9 @@ namespace CarLotSim
         {
             Console.WriteLine($"Cars in lot: {CarLot.numberOfCars}");
             var car = new Car();
+            Console.WriteLine($"Cars in lot: {CarLot.numberOfCars}");
             var theirCar = new Car() { Year = 1997, Make = "Ford", Model = "F-150"};
+            Console.WriteLine($"Cars in lot: {CarLot.numberOfCars}");
             var newCar = new Car(2010, "Nissan", "Santra");
             Console.WriteLine($"Cars in lot: {CarLot.numberOfCars}");
         }
